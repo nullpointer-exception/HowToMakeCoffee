65496d970f450dba797336c8e7906f5e1cebff4e
Bea <nllptr-exception@protonmail.com>
2023-05-18 17:48:57 +0200

    [ADD] gitignore

---
+++ HowToMakeCoffee/Source/HowToMakeCoffee/HowToMakeCoffeeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HowToMakeCoffeeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOWTOMAKECOFFEE_API AHowToMakeCoffeeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
